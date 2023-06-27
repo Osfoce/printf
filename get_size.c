96465b47de7c33587d7ae397ab0afa6fa25aa761
Osfoce <oselufortunatus2002@gmail.com>
2023-06-27 23:43:09 +0100

    calc the size to cast the arg...

--- get_size.c
+++ get_size.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
+ * get_size - calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
@@ -21,6 +21,4 @@ int get_size(const char *format, int *i)
 		*i = curr_i - 1;
 	else
 		*i = curr_i;
-
-	return (size);
 }
