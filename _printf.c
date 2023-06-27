668fab43bc57715991c215933c2322470a286eb4
Osfoce <oselufortunatus2002@gmail.com>
2023-06-27 22:40:01 +0100

    prints to the console

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * Description: this function will call the get_print() function that will
  * determine which printing function to call depending on the conversion
  * specifiers contained into fmt
- * Return: length of the formatted output string
+ * Return: returns length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
